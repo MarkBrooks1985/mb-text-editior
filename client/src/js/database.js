2d45d93c07dab9e1ef4e1bde3b5f2a12b7821d20
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-17 13:35:04 +1030

    3rd commit, put logic and method added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,30 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.log("PUT Request to update JATE DB");
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  const objectStore = tx.objectStore("jate");
+  const req = objStore.put({ id: id, value: value });
+  const res = await req;
+  console.log("DATA saved to jate DB", res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
